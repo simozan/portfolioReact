2767675de369dce30efa4e0354c5c80b796d5fb0
Simone <simone.zanni.87@gmail.com>
2024-10-08 09:49:28 +0200

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ export const About = () => {
       <Navbar />
       <section className="main margin-top-50">
         <h1 className="main-title margin-bottom-35">I'm Simone</h1>
-        <div className="main-container">
+        {/* <div className="main-container"> */}
           {/* <img
             className="profile-img"
             src="../assets/hero/simo.png"
@@ -21,32 +21,28 @@ export const About = () => {
           <ul className="aboutItems">
             <li className="aboutItem">
              
-              <div className="aboutItemText">
                 <h3>Frontend Developer</h3>
                 <p>
                   I'm a frontend developer with experience in building
                   responsive and optimize site
                 </p>
-              </div>
             </li>
             <li className="aboutItem">
              
-              <div className="aboutItem">
                 <h3>Backend Developer</h3>
                 <p>I have experience developing fast and optimised API</p>
-              </div>
             </li>
-            <li className="aboutItem">
-              {/* <div className="aboutItemText">
+            {/* <li className="aboutItem">
+              <div className="aboutItemText">
                 <h3>Salesforce</h3>
                 <p>
                   I have designed multiple landing pages and have created design
                   system as well
                 </p>
-              </div> */}
-            </li>
+              </div>
+            </li> */}
           </ul>
-        </div>
+        {/* </div> */}
         <div>
           <div>
             <h3 className="about-title">Skills</h3>
